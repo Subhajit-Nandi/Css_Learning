60bdf55aa1297f46e86b10c84abdda7fd6d8cdff
XAVIER <subhajitnandi1343@gmail.com>
2023-09-06 10:25:25 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors:{
+        chestnut: '#973f29'
+      }
+    },
+  },
+  plugins: [],
+}
